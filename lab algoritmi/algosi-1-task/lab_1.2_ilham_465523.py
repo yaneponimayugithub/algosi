4ab1635871bea24f6b2f3e980ec831b1450c8633
yaneponimayugithub <465523@niuitmo.ru>
2024-09-21 15:47:38 +0300

    {optimiz} input a and b

--- lab algoritmi/algosi-1-task/lab_1.2_ilham_465523.py
+++ lab algoritmi/algosi-1-task/lab_1.2_ilham_465523.py
@@ -1,4 +1,4 @@
-a,b=int(input()),int(input())
+a, b = map(int ,input().split())
 if a > 10**9 or a < -10**9 or b > 10**9 or b < -10**9:
     print("число должно быть в диапазоне от -10^9 до 10^9")
 else:
