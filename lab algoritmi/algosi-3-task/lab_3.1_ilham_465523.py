d321ae594f542a08d90eee702e061eb11e414f7a
yaneponimayugithub <465523@niuitmo.ru>
2024-09-23 18:03:36 +0300

    .

--- lab algoritmi/algosi-3-task/lab_3.1_ilham_465523.py
+++ lab algoritmi/algosi-3-task/lab_3.1_ilham_465523.py
@@ -1,14 +1,17 @@
-a = open('input.txt')
+a = open('input.txt', 'r')
 b = a.read()
-b = int(b)
 a.close()
+
+b = int(b)
+
 def fibonacci(number):
     if number <= 1:
         return number
     return fibonacci(number - 1) + fibonacci(number - 2)
+
 if b < 0 or b > 10**7:
     print("Диапазон чисел нарушен")
 else:
     m = open('output.txt', 'w')
-    m.write(str(fibonacci(b))[-1])
+    m.write(str(fibonacci(b) % 10))
     m.close()
